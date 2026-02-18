96394af0f239c0a78f204d25d2112a0eb59cb80d
Krisna Alifandi Tristio <krt@ogloba.com>
2026-02-18 16:40:01 +0700

    Krisna - Frontend initialization

---
+++ frontend/eslint.config.js
@@ -0,0 +1,30 @@
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules', 'eslint.config.js'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Browser globals for React
+      },
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react/react-in-jsx-scope': 'off', // Not needed for modern React
+    },
+  },
+);
