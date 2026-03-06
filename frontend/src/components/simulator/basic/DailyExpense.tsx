78c2cb04bcdd39166c7f2b692db741e1fdb7230c
krisna-ogloba <krt.github@ogloba.com>
2026-03-06 16:08:15 +0700

    fix daily expense

--- frontend/src/components/simulator/basic/DailyExpense.tsx
+++ frontend/src/components/simulator/basic/DailyExpense.tsx
@@ -41,9 +41,20 @@ export default function DailyExpense({
             >
               {isMobile ? (
                 <div className="flex flex-col items-start gap-3 flex-1">
-                  <div className="w-14 h-14 rounded-lg flex gap-2 items-center text-lg shrink-0">
-                    <img src={category.icon} alt={category.name} />
-                    <h4 className="text-sm">{category.name}</h4>
+                  <div className="flex flex-wrap justify-between w-full">
+                    <div className="w-14 h-14 rounded-lg flex gap-2 items-center text-lg shrink-0">
+                      <img src={category.icon} alt={category.name} />
+                      <h4 className="text-sm">{category.name}</h4>
+                    </div>
+                    <div className="text-right ">
+                      <Input
+                        borderClass="border-[#006F73]"
+                        textClass="text-[#006F73]"
+                        value={expenses[category.id]}
+                        onChange={(value) => onChange(category.id, value)}
+                        unit={'€'}
+                      />
+                    </div>
                   </div>
                   <div className="flex items-center flex-wrap gap-2">
                     {category.partners.map((partner, index) => (
@@ -69,47 +80,48 @@ export default function DailyExpense({
                   </div>
                 </div>
               ) : (
-                <div className="flex justify-start items-center gap-3 flex-1">
-                  <div className="w-14 h-14 rounded-lg flex gap-2 items-center text-lg shrink-0">
-                    <img src={category.icon} alt={category.name} />
-                  </div>
-                  <div>
-                    <h4 className="text-sm">{category.name}</h4>
-                    <div className="flex items-center flex-wrap gap-2">
-                      {category.partners.map((partner, index) => (
-                        <div
-                          key={index}
-                          className="flex items-center gap-1.5 rounded-[10px] p-1 text-[10px] bg-white"
-                        >
-                          <img
-                            src={partner.icon}
-                            alt={partner.name}
-                            className="w-7.5 h-7.5"
-                          />
-                          <div className="flex flex-col gap-0.75">
-                            <p className="font-semibold text-[10px]">
-                              {partner.name}
-                            </p>
-                            <div className="bg-[#E6F1F1] text-[10px] w-fit p-0.5 rounded-lg font-bold text-[#006F73]">
-                              <p>{partner.percentage}%</p>
+                <>
+                  <div className="flex justify-start items-center gap-3 flex-1">
+                    <div className="w-14 h-14 rounded-lg flex gap-2 items-center text-lg shrink-0">
+                      <img src={category.icon} alt={category.name} />
+                    </div>
+                    <div>
+                      <h4 className="text-sm">{category.name}</h4>
+                      <div className="flex items-center flex-wrap gap-2">
+                        {category.partners.map((partner, index) => (
+                          <div
+                            key={index}
+                            className="flex items-center gap-1.5 rounded-[10px] p-1 text-[10px] bg-white"
+                          >
+                            <img
+                              src={partner.icon}
+                              alt={partner.name}
+                              className="w-7.5 h-7.5"
+                            />
+                            <div className="flex flex-col gap-0.75">
+                              <p className="font-semibold text-[10px]">
+                                {partner.name}
+                              </p>
+                              <div className="bg-[#E6F1F1] text-[10px] w-fit p-0.5 rounded-lg font-bold text-[#006F73]">
+                                <p>{partner.percentage}%</p>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      ))}
+                        ))}
+                      </div>
                     </div>
                   </div>
-                </div>
+                  <div className="text-right ">
+                    <Input
+                      borderClass="border-[#006F73]"
+                      textClass="text-[#006F73]"
+                      value={expenses[category.id]}
+                      onChange={(value) => onChange(category.id, value)}
+                      unit={'€'}
+                    />
+                  </div>
+                </>
               )}
-
-              <div className="text-right ">
-                <Input
-                  borderClass="border-[#006F73]"
-                  textClass="text-[#006F73]"
-                  value={expenses[category.id]}
-                  onChange={(value) => onChange(category.id, value)}
-                  unit={'€'}
-                />
-              </div>
             </div>
           </div>
         ))}
